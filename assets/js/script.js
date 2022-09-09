6d68a2ac4554642755304e9805dfb9fe83dd579b
Rodrigo Barros <rodrigobarros2802@gmail.com>
2022-09-09 15:33:18 -0300

    Editando script js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,17 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'vertical',
-    loop: true,
-  
-    // If we need pagination
+const swiper = new Swiper('.swiper-container', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+    },
     pagination: {
       el: '.swiper-pagination',
     },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
+    loop: true,
   });
